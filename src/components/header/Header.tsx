0570b53a1a9f8260613338a7b915772dc3697aae
pedrodevsz <pedrodevs25@gmail.com>
2025-09-13 14:37:27 -0300

    feat: create header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import { Link } from "lucide-react";
+
+export function Header() {
+    return (
+        <header className="flex justify-center text-base font-semibold gap-x-4">
+            <Link href="/user/auth/login">Login</Link>
+            <Link href="/user/auth/register">Cadastro</Link>
+        </header>
+    )
+}
